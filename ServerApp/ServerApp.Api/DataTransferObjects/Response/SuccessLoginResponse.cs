c04d8c1f9122a9fd03ddbb5c142a6752d1eb576e
Dmitry Prudnikov <dimahudric@gmail.com>
2024-04-20 11:48:17 +0300

    add endpnts + refactoring

---
+++ ServerApp/ServerApp.Api/DataTransferObjects/Response/SuccessLoginResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace ServerApp.Api.DataTransferObjects.Response;
+
+public record class SuccessLoginResponse(string Token);
+public record class UserDataResponse(string Id, ulong TelegramId, string? Name, string Bio, string Login, string PhotoBase64, DateTime RegistrationDate, int Age, double Latitude, double Longtitude, bool IsLinkWeb, bool IsLinkTelegram, uint FriendsCount);
---
+++ ServerApp/ServerApp.Api/RouteManager.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerApp.Api.DataTransferObjects.Request;
+using ServerApp.Api.DataTransferObjects.Response;
+namespace ServerApp.Api;
+
+internal static class RouteManager {
+    private static WebApplication app = null!;
+
+    public static void SetEndpoints(WebApplication app) {
+        RouteManager.app = app;
+
+        Auth();
+        User();
+    }
+
+    public static void Auth() {
+        _ = app.MapPost("/auth/registration", (RegistrationRequest dto) => {
+            return Results.NotFound($"201 - if account created; 403 - if login already exists");
+        });
+
+        _ = app.MapPost("/auth/login", (LoginRequest dto) => {
+            _ = Results.NotFound("200 - succesfull login and returned SuccesLoginResponse dto. refresh token will be in `X-Refresh` cookie; 400 - something went wrong; 403 - wrong password");
+        });
+
+        _ = app.MapGet("/auth/refresh", () => Results.NotFound("200 - if cookie has a X-Refresh with a valide value and return SuccessLoginResponse dto, oterwise 403"));
+
+        _ = app.MapPost("/auth/registration/telegram", (TelegramRegistrationRequest dto) => Results.NotFound("always 200 and returns SuccessLoginResponse dto"));
+
+        _ = app.MapPut("/auth/sync/telegram", (TelegramSyncRequest dto) => Results.NotFound("always 200"));
+
+
+
+        //
+        _ = app.MapGet("/forswagger/dont/send/this/anywhere", (
+                    [FromBody] SuccessLoginResponse one
+                    ) => Results.NotFound());
+    }
+
+    public static void User() {
+        _ app.MapGet("/user/{id}", (long id) => Results.NotFound(""))
+    }
+}
