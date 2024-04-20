f44706f0fb8a89f049a9fa087a0efa86bab1f5b0
Dmitry Prudnikov <dimahudric@gmail.com>
2024-04-20 08:12:18 +0300

    add migrations and other things, i'm not sure

--- ServerApp/ServerApp.Api/Program.cs
+++ ServerApp/ServerApp.Api/Program.cs
@@ -1,4 +1,8 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using NetTopologySuite.Geometries;
+using ServerApp.Logic;
+using ServerApp.Logic.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,6 +16,61 @@ if (app.Environment.IsDevelopment()) {
     _ = app.UseSwagger();
     _ = app.UseSwaggerUI();
 }
+
+// using var db = new ApplicationContext();
+// var user = new User {
+//     Name = "asd",
+//     Login = "john.doe",
+//     Password = "password123",
+//     PhotoBase64 = "photoBase64String",
+//     RegistrationDate = DateTime.Now,
+//     Age = 30,
+//     Role = new Role("User"),
+//     SearchGeoposition = new Point(40.7128, -74.0060),
+// };
+//
+// User user1 = new User {
+//     Name = "asd",
+//     Login = "john.doe",
+//     Password = "password123",
+//     PhotoBase64 = "photoBase64String",
+//     RegistrationDate = DateTime.Now,
+//     Age = 30,
+//     Role = new Role("User"),
+//     SearchGeoposition = new Point(40.7128, -74.0060),
+// };
+//
+// var hobby1 = new Hobby {
+//     Name = "Hiking",
+// };
+//
+// var friendsPair1 = new FriendsPair {
+//     Sender = user1,
+//     Reciever = user
+// };
+// var role1 = new Role("Admin") { };
+// var activity1 = new Activity {
+//     Id = 1,
+//     Name = "Hiking in the mountains",
+//     Geoposition = new Point(39.5501, -105.7821),
+//     CreationTime = DateTime.Now,
+//     WorkBegin = DateTime.Now,
+//     WorkEnd = DateTime.Now,
+//     CancelAge = false,
+// };
+// var activityType1 = new ActivityType {
+//     Name = "Outdoor",
+//     Decription = "Activities in the open air",
+// };
+//
+// db.Activities.AddRange(activity1);
+// db.ActivityTypes.AddRange(activityType1);
+// db.Roles.AddRange(role1);
+// db.FriendsPairs.AddRange(friendsPair1);
+// db.Hobbies.AddRange(hobby1);
+// db.Users.AddRange(user, user1);
+//
+// db.SaveChanges();
 
 app.UseHttpsRedirection();
 
