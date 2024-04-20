697ac322eef96e41b2fea9f081f89cf45b054c19
Dmitry Prudnikov <dimahudric@gmail.com>
2024-04-20 13:18:27 +0300

    more endpnts

--- ServerApp/ServerApp.Api/DataTransferObjects/Response/SuccessLoginResponse.cs
+++ ServerApp/ServerApp.Api/DataTransferObjects/Response/SuccessLoginResponse.cs
@@ -2,3 +2,4 @@
 
 public record class SuccessLoginResponse(string Token);
 public record class UserDataResponse(string Id, ulong TelegramId, string? Name, string Bio, string Login, string PhotoBase64, DateTime RegistrationDate, int Age, double Latitude, double Longtitude, bool IsLinkWeb, bool IsLinkTelegram, uint FriendsCount);
+public record class FriendsResponse(List<UserDataResponse> Friends);
