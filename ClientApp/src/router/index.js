3ca2145d97bb374b0296856cb11efb5f544a2d93
danilPorkchop15 <danilkostin150705@gmail.com>
2024-04-20 16:00:17 +0300

    pinia stores created

--- ClientApp/src/router/index.js
+++ ClientApp/src/router/index.js
@@ -6,15 +6,47 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      // component: HomeView
+      component: () => import('../views/HomeView.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import('../views/AboutView.vue')
+      path: '/map',
+      name: 'map',
+      component: () => import('../views/MapView.vue')
+    },
+    {
+      path: '/friends',
+      name: 'friends',
+      component: () => import('../views/FriendsView.vue')
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../views/ProfileView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue')
+    },
+    {
+      path: '/hobby-picker',
+      name: 'hobby-picker',
+      component: () => import('../views/HobbyPickerView.vue')
+    },
+    {
+      path: '/routes',
+      name: 'routes',
+      component: () => import('../views/RoutesView.vue')
+    },
+    {
+      path: ':pathMatch(.*)*',
+      name: 'notFound',
+      component: () => import('../views/NotFoundView.vue')
     }
   ]
 })
